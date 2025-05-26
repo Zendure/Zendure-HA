0d99083cb868efd5624782597c29dd3880cdceb1
harrymayr <104222469+harrymayr@users.noreply.github.com>
2025-05-26 21:12:24 +0200

    Update solarflow2400ac.py
    Remove "old" remaining times

--- custom_components/zendure_ha/devices/solarflow2400ac.py
+++ custom_components/zendure_ha/devices/solarflow2400ac.py
@@ -56,8 +56,6 @@ class SolarFlow2400AC(ZendureDevice):
             self.sensor("packInputPower", None, "W", "power", "measurement"),
             self.sensor("outputPackPower", None, "W", "power", "measurement"),
             self.sensor("outputHomePower", None, "W", "power", "measurement"),
-            self.sensor("remainOutTime", "{{ (value / 60) }}", "h", "duration"),
-            self.sensor("remainInputTime", "{{ (value / 60) }}", "h", "duration"),
             self.sensor("packNum", None),
             self.sensor("electricLevel", None, "%", "battery"),
             self.sensor("energyPower", None, "W"),
@@ -69,6 +67,9 @@ class SolarFlow2400AC(ZendureDevice):
             self.sensor("strength", None),
         ]
         ZendureSensor.add(sensors)
+
+        self.nosensor(["remainOutTime"])
+        self.nosensor(["remainInputTime"])
 
         selects = [self.select("acMode", {1: "input", 2: "output"}, self.update_ac_mode)]
         ZendureSelect.add(selects)
