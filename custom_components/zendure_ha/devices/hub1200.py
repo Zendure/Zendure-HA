0c8b2124f80412e4ed62a4c01b420d938b7c276c
harrymayr <104222469+harrymayr@users.noreply.github.com>
2025-08-02 23:54:37 +0200

    Update hub1200.py
    Fix passMode

--- custom_components/zendure_ha/devices/hub1200.py
+++ custom_components/zendure_ha/devices/hub1200.py
@@ -7,6 +7,7 @@ from homeassistant.core import HomeAssistant
 
 from custom_components.zendure_ha.const import ManagerState
 from custom_components.zendure_ha.device import ZendureBattery, ZendureLegacy
+from custom_components.zendure_ha.select import ZendureRestoreSelect
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -17,6 +18,7 @@ class Hub1200(ZendureLegacy):
         super().__init__(hass, deviceId, definition["deviceName"], prodName, definition)
         self.powerMin = -800
         self.powerMax = 800
+        self.passMode = ZendureRestoreSelect(self, "passMode", {0: "auto", 2: "on", 1: "off"}, self.entityWrite, 1)
 
     def batteryUpdate(self, batteries: list[ZendureBattery]) -> None:
         # Check if any battery has kWh > 1
