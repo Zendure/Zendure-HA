0e3d83d5142b7a1f9e02bb6a41bfa2ca80136f75
harrymayr <104222469+harrymayr@users.noreply.github.com>
2025-08-03 00:50:08 +0200

    Update solarflow800.py
    move gridReverse to device

--- custom_components/zendure_ha/devices/solarflow800.py
+++ custom_components/zendure_ha/devices/solarflow800.py
@@ -6,6 +6,7 @@ from typing import Any
 from homeassistant.core import HomeAssistant
 
 from custom_components.zendure_ha.device import ZendureZenSdk
+from custom_components.zendure_ha.select import ZendureRestoreSelect
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -16,3 +17,4 @@ class SolarFlow800(ZendureZenSdk):
         super().__init__(hass, deviceId, definition["deviceName"], prodName, definition)
         self.powerMin = -1200
         self.powerMax = 800
+        self.gridReverse = ZendureRestoreSelect(self, "gridReverse", {0: "auto", 1: "on", 2: "off"}, self.entityWrite, 1)
