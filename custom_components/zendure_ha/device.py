0e0acb36a9afa6d7e01b1cc4bde33900810e1b4d
harrymayr <104222469+harrymayr@users.noreply.github.com>
2025-08-02 10:32:24 +0200

    Update device.py
    Update slider limitInput according to device chargeLimit (see #515)

--- custom_components/zendure_ha/device.py
+++ custom_components/zendure_ha/device.py
@@ -129,6 +129,9 @@ class ZendureDevice(EntityDevice):
                 case "inverseMaxPower":
                     self.powerMax = value
                     self.limitOutput.update_range(0, value)
+                case "chargeLimit":
+                    self.powerMin = -value
+                    self.limitInput.update_range(0, value)
 
         return changed
 
