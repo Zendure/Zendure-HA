bebc5d3e28562bb5847e70d1a27267700b657def
harrymayr <104222469+harrymayr@users.noreply.github.com>
2026-02-14 15:05:49 +0100

    Adjust power limits for Hub1200 device
    Updated power limits for Hub1200 to support 900W AC input of ACE 1500

--- custom_components/zendure_ha/devices/hub1200.py
+++ custom_components/zendure_ha/devices/hub1200.py
@@ -14,7 +14,8 @@ class Hub1200(ZendureLegacy):
     def __init__(self, hass: HomeAssistant, deviceId: str, prodName: str, definition: Any) -> None:
         """Initialise Hub1200."""
         super().__init__(hass, deviceId, definition["deviceName"], prodName, definition)
-        self.setLimits(-800, 800)
+        """Can control the ACE 1500 with max 900W AC inputPower"""
+        self.setLimits(-900, 1200)
         self.maxSolar = -800
 
     def batteryUpdate(self, batteries: list[ZendureBattery]) -> None:
